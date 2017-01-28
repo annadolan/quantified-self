307ade139a7d7a37dadcac124b6bc69c3dfb2c22
Jon Kimble <jonathanbkimble@gmail.com>
2017-01-28 10:56:10 -0700

    Change initial timeout

--- test/exercise-test.js
+++ test/exercise-test.js
@@ -4,7 +4,7 @@ var test      = require('selenium-webdriver/testing');
 
 test.describe('testing quantified self', function() {
     var driver;
-  this.timeout(1000000);
+  this.timeout(10000);
 
   test.beforeEach(function() {
     driver = new webdriver.Builder()
