9ed5b20214c226ff54d95fec34d9d53532b60308
Anna Dolan <anna.dolan@gmail.com>
2017-01-29 20:38:22 -0700

    Refactor on edit and filter complete, tests passing

--- main.bundle.js
+++ main.bundle.js
@@ -264,7 +264,8 @@
 	  } else {
 	    this.clearFields();
 	    this.storeFoods();
-	    this.buildTable();
+	    $('#foods-table').empty();
+	    this.displayFoods();
 	  }
 	};
 
@@ -278,21 +279,28 @@
 	    var name = $(this).parent().siblings()[0].innerHTML;
 	    var calories = $(this).parent().siblings()[1].innerHTML;
 	    $(this).parent().parent().remove();
-	    deleteFood(name, calories);
+	    var newFood = new Food(name, calories);
+	    newFood.deleteFood();
 	  });
 
 	  $(".calorie-cell").on('click', function () {
 	    var key = "calories";
-	    editValue(this, key);
+	    var $calories = $(this).html();
+	    var $name = $(this).siblings().html();
+	    editFood = new Food($name, $calories);
+	    editFood.editValue(this, key);
 	  });
 
 	  $(".name-cell").on('click', function () {
 	    var key = "name";
-	    editValue(this, key);
+	    var $name = $(this).html();
+	    var $calories = $(this).siblings().html();
+	    editFood = new Food($name, $calories);
+	    editFood.editValue(this, key);
 	  });
 
 	  $("#filter").keyup(function () {
-	    filterFoods();
+	    filterItems(foodsTable, this);
 	  });
 	});
 
@@ -301,7 +309,9 @@
 	  newFood.addFood();
 	});
 
-	function deleteFood(name, calories) {
+	Food.prototype.deleteFood = function () {
+	  var name = this.name;
+	  var calories = this.calories;
 	  var foods = JSON.parse(localStorage.getItem('foods'));
 	  foods.forEach(function (object) {
 	    if (object.name === name && object.calories === calories) {
@@ -310,7 +320,27 @@
 	    foodJSON = JSON.stringify(foods);
 	    localStorage.setItem('foods', foodJSON);
 	  });
-	}
+	};
+
+	Food.prototype.editValue = function (obj, key) {
+	  var that = this;
+	  obj.contentEditable = true;
+	  clickedEntry = obj;
+	  var originalKey = obj.innerText;
+	  event.stopPropagation();
+	  $(obj).keydown(function (event) {
+	    if (event.keyCode == 13) {
+	      var newKey = obj.innerText;
+	      obj.contentEditable = false;
+	      that.setLocalStorage(key, newKey);
+	    }
+	  });
+	  $(document).click(function (e) {
+	    var newKey = clickedEntry.innerText;
+	    clickedEntry.contentEditable = false;
+	    that.setLocalStorage(key, newKey);
+	  });
+	};
 
 	Food.prototype.emptyErrors = function () {
 	  $("#food-error").empty();
@@ -321,37 +351,6 @@
 	  $('#foodname').val("");
 	  $('#caloriecount').val("");
 	};
-
-	//
-	// Food.prototype.buildTable = function() {
-	//   var newRow = document.createElement('tr');
-	//   buildNameCell(this.name, newRow);
-	//   buildCalorieCell(this.calories, newRow);
-	//   buildTrashCell(newRow);
-	//   foodsTable.insertBefore(newRow, foodsTable.children[1]);
-	// }
-	//
-	// function buildNameCell(name, newRow){
-	//   var nameCell = document.createElement('td');
-	//   nameCell.innerText = name;
-	//   nameCell.className = "name-cell";
-	//   nameCell.id = "food-name-cell";
-	//   newRow.appendChild(nameCell);
-	// }
-	//
-	// function buildCalorieCell(calories, newRow){
-	//   var calorieCell = document.createElement('td');
-	//   calorieCell.innerText = calories;
-	//   calorieCell.className = "calorie-cell";
-	//   calorieCell.id = "food-calorie-cell";
-	//   newRow.appendChild(calorieCell);
-	// }
-	//
-	// function buildTrashCell(newRow){
-	//   var trashCell = document.createElement('td');
-	//   trashCell.innerHTML = "<a href='' id='trash-icon'><span class='glyphicon glyphicon-trash trash-icon'></a>";
-	//   newRow.appendChild(trashCell);
-	// }
 
 	Food.prototype.storeFoods = function () {
 	  var foodDataJSON = localStorage.getItem('foods');
@@ -367,72 +366,31 @@
 	Food.prototype.displayFoods = function () {
 	  var foods = JSON.parse(localStorage.getItem('foods'));
 	  foods.forEach(function (object) {
-	    $('foods-table').append(`<tr>
+	    $('#foods-table tr:first-child').after(`<tr>
 	      <td class='name-cell' id='food-name-cell'>${object.name}</td>
 	      <td class='calorie-cell' id='food-calorie-cell'>${object.calories}</td>
 	      <td><a href='' id='trash-icon'><span class='glyphicon glyphicon-trash trash-icon'></a></td>
 	      </tr>`);
 	  });
-	  debugger;
-	  // JSON.parse(localStorage.getItem('foods')).forEach(function(element){
-	  //   element.buildTable();
-	  // });
 	};
 
-	function editValue(obj, key) {
-	  obj.contentEditable = true;
-	  clickedEntry = obj;
-	  var originalKey = obj.innerText;
-	  event.stopPropagation();
-	  $(obj).keydown(function (event) {
-	    if (event.keyCode == 13) {
-	      var newKey = obj.innerText;
-	      obj.contentEditable = false;
-	      if (key === "name") {
-	        setNameLocalStorage(originalKey, newKey);
-	      } else {
-	        setCalsLocalStorage(originalKey, newKey);
-	      }
-	    }
-	  });
-	  $(document).click(function (e) {
-	    var newKey = clickedEntry.innerText;
-	    clickedEntry.contentEditable = false;
-	    if (key === "name") {
-	      setNameLocalStorage(originalKey, newKey);
-	    } else {
-	      setCalsLocalStorage(originalKey, newKey);
-	    }
-	  });
-	}
-
-	function setNameLocalStorage(originalKey, newKey) {
+	Food.prototype.setLocalStorage = function (key, newKey) {
 	  var food = JSON.parse(localStorage.getItem('foods'));
-	  food.forEach(function (object, key) {
-	    if (object.name == originalKey) {
-	      object.name = newKey;
+	  var name = this.name;
+	  var calories = this.calories;
+	  food.forEach(function (object) {
+	    if (object.name === name && object.calories === calories) {
+	      object[key] = newKey;
 	    }
 	    foodJSON = JSON.stringify(food);
 	    localStorage.setItem('foods', foodJSON);
 	  });
-	}
+	};
 
-	function setCalsLocalStorage(originalKey, newKey) {
-	  var food = JSON.parse(localStorage.getItem('foods'));
-	  food.forEach(function (object, key) {
-	    if (object.calories == originalKey) {
-	      object.calories = newKey;
-	    }
-	    foodJSON = JSON.stringify(food);
-	    localStorage.setItem('foods', foodJSON);
-	  });
-	}
-
-	function filterFoods() {
+	function filterItems(table, input) {
 	  var input, filter, table, tr, td, i;
-	  input = document.getElementById("filter");
 	  filter = input.value.toUpperCase();
-	  tr = foodsTable.getElementsByTagName("tr");
+	  tr = table.getElementsByTagName("tr");
 
 	  for (i = 0; i < tr.length; i++) {
 	    td = tr[i].getElementsByTagName("td")[0];
