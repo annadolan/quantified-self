1c6cfeda940d7fd36d587d9ec0c2bf415febe154
Anna Dolan <anna.dolan@gmail.com>
2017-01-30 10:29:31 -0700

    Make tests pass

--- main.bundle.js
+++ main.bundle.js
@@ -47,8 +47,7 @@
 	var diaryFoodsTable = document.getElementById('diary-foods-table');
 	var diaryExercisesTable = document.getElementById('diary-exercises-table');
 	var Food = __webpack_require__(1);
-	var Exercise = __webpack_require__(2);
-	var Reset = __webpack_require__(3);
+	var Exercise = __webpack_require__(5);
 	var addExerciseButton = document.getElementById('add-exercise');
 
 	function displayFoodData() {
@@ -475,11 +474,14 @@
 
 /***/ },
 /* 1 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	var foodsTable = document.getElementById('foods-table');
 	var trashIcon = document.getElementById('trash-icon');
 	var foodForm = document.getElementById('food-form');
+	var Reset = __webpack_require__(2);
+	var Save = __webpack_require__(3);
+	var View = __webpack_require__(4);
 
 	function Food(name, calories) {
 	  this.name = name;
@@ -498,9 +500,67 @@
 	  } else {
 	    reset.clearFields('#foodname', '#caloriecount');
 	    this.storeFoods();
-	    $('#foods-table tr:first-child').nextAll().empty();
+	    reset.clearTable(foodsTable);
 	    this.displayFoods();
 	  }
+	};
+
+	Food.prototype.deleteFood = function () {
+	  var name = this.name;
+	  var calories = this.calories;
+	  var foods = JSON.parse(localStorage.getItem('foods'));
+	  foods.forEach(function (object) {
+	    if (object.name === name && object.calories === calories) {
+	      foods.splice(foods.indexOf(object), 1);
+	    }
+	    foodJSON = JSON.stringify(foods);
+	    localStorage.setItem('foods', foodJSON);
+	  });
+	};
+
+	Food.prototype.editValue = function (obj, key) {
+	  var name = this.name;
+	  var calories = this.calories;
+	  var type = 'foods';
+	  var save = new Save();
+	  obj.contentEditable = true;
+	  clickedEntry = obj;
+	  var originalKey = obj.innerText;
+	  event.stopPropagation();
+	  $(obj).keydown(function (event) {
+	    if (event.keyCode == 13) {
+	      var newKey = obj.innerText;
+	      obj.contentEditable = false;
+	      save.setLocalStorage(key, newKey, name, calories, type);
+	    }
+	  });
+	  $(document).click(function (e) {
+	    var newKey = clickedEntry.innerText;
+	    clickedEntry.contentEditable = false;
+	    save.setLocalStorage(key, newKey, name, calories, type);
+	  });
+	};
+
+	Food.prototype.storeFoods = function () {
+	  var foodDataJSON = localStorage.getItem('foods');
+	  if (foodDataJSON === null) {
+	    foodDataJSON = '[]';
+	  }
+	  var foods = JSON.parse(foodDataJSON);
+	  foods.push(this);
+	  foodDataJSON = JSON.stringify(foods);
+	  localStorage.setItem('foods', foodDataJSON);
+	};
+
+	Food.prototype.displayFoods = function () {
+	  var foods = JSON.parse(localStorage.getItem('foods'));
+	  foods.forEach(function (object) {
+	    $('#foods-table tr:first-child').after(`<tr>
+	      <td class='name-cell' id='food-name-cell'>${object.name}</td>
+	      <td class='calorie-cell' id='food-calorie-cell'>${object.calories}</td>
+	      <td><a href='' id='trash-icon'><span class='glyphicon glyphicon-trash trash-icon'></a></td>
+	      </tr>`);
+	  });
 	};
 
 	$(document).ready(function () {
@@ -534,7 +594,8 @@
 	  });
 
 	  $("#filter").keyup(function () {
-	    filterItems(foodsTable, this);
+	    view = new View();
+	    view.filterItems(foodsTable, this);
 	  });
 	});
 
@@ -543,73 +604,36 @@
 	  newFood.addFood();
 	});
 
-	Food.prototype.deleteFood = function () {
-	  var name = this.name;
-	  var calories = this.calories;
-	  var foods = JSON.parse(localStorage.getItem('foods'));
-	  foods.forEach(function (object) {
-	    if (object.name === name && object.calories === calories) {
-	      foods.splice(foods.indexOf(object), 1);
-	    }
-	    foodJSON = JSON.stringify(foods);
-	    localStorage.setItem('foods', foodJSON);
-	  });
+	module.exports = Food;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	function Reset() {}
+
+	Reset.prototype.emptyErrors = function (idName) {
+	  $(idName).children().children().empty();
 	};
 
-	Food.prototype.editValue = function (obj, key) {
-	  var name = this.name;
-	  var calories = this.calories;
-	  var type = 'foods';
-	  obj.contentEditable = true;
-	  clickedEntry = obj;
-	  var originalKey = obj.innerText;
-	  event.stopPropagation();
-	  $(obj).keydown(function (event) {
-	    if (event.keyCode == 13) {
-	      var newKey = obj.innerText;
-	      obj.contentEditable = false;
-	      setLocalStorage(key, newKey, name, calories, type);
-	    }
-	  });
-	  $(document).click(function (e) {
-	    var newKey = clickedEntry.innerText;
-	    clickedEntry.contentEditable = false;
-	    setLocalStorage(key, newKey, name, calories, type);
-	  });
+	Reset.prototype.clearFields = function (field1, field2) {
+	  $(field1).val("");
+	  $(field2).val("");
 	};
 
-	// function emptyErrors(idName){
-	//   $(idName).children().children().empty();
-	// }
-	//
-	// function clearFields(field1, field2){
-	//   $(field1).val("");
-	//   $(field2).val("");
-	// }
-
-	Food.prototype.storeFoods = function () {
-	  var foodDataJSON = localStorage.getItem('foods');
-	  if (foodDataJSON === null) {
-	    foodDataJSON = '[]';
-	  }
-	  var foods = JSON.parse(foodDataJSON);
-	  foods.push(this);
-	  foodDataJSON = JSON.stringify(foods);
-	  localStorage.setItem('foods', foodDataJSON);
+	Reset.prototype.clearTable = function (table) {
+	  $(`#${table.id} tr:first-child`).nextAll().empty();
 	};
 
-	Food.prototype.displayFoods = function () {
-	  var foods = JSON.parse(localStorage.getItem('foods'));
-	  foods.forEach(function (object) {
-	    $('#foods-table tr:first-child').after(`<tr>
-	      <td class='name-cell' id='food-name-cell'>${object.name}</td>
-	      <td class='calorie-cell' id='food-calorie-cell'>${object.calories}</td>
-	      <td><a href='' id='trash-icon'><span class='glyphicon glyphicon-trash trash-icon'></a></td>
-	      </tr>`);
-	  });
-	};
+	module.exports = Reset;
 
-	function setLocalStorage(key, newKey, name, calories, type) {
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	function Save() {}
+
+	Save.prototype.setLocalStorage = function (key, newKey, name, calories, type) {
 	  var array = JSON.parse(localStorage.getItem(type));
 	  array.forEach(function (object) {
 	    if (object.name === name && object.calories === calories) {
@@ -618,9 +642,17 @@
 	    arrayJSON = JSON.stringify(array);
 	    localStorage.setItem(type, arrayJSON);
 	  });
-	}
+	};
 
-	function filterItems(table, input) {
+	module.exports = Save;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	function View() {}
+
+	View.prototype.filterItems = function (table, input) {
 	  var input, filter, table, tr, td, i;
 	  filter = input.value.toUpperCase();
 	  tr = table.getElementsByTagName("tr");
@@ -635,138 +667,61 @@
 	      }
 	    }
 	  }
-	}
+	};
 
-	module.exports = Food;
+	module.exports = View;
 
 /***/ },
-/* 2 */
-/***/ function(module, exports) {
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
 
 	var exercisesTable = document.getElementById('all-exercises-table');
 	var trashIcon = document.getElementById('trash-icon-ex');
+	var exerciseForm = document.getElementById('exercise-form');
+	var Reset = __webpack_require__(2);
+	var Save = __webpack_require__(3);
+	var View = __webpack_require__(4);
 
-	function Exercise(exerciseName, calorieCount) {
-	  this.name = exerciseName;
-	  this.calories = calorieCount;
-	  this.build = function () {
-	    buildTableEx(exerciseName, calorieCount);
-	  };
-	  this.store = function () {
-	    storeExercises(exerciseName, calorieCount);
-	  };
+	function Exercise(name, calories) {
+	  this.name = name;
+	  this.calories = calories;
 	}
 
-	$(document).ready(function () {
-	  displayExercises();
-
-	  $(document).on("click", "#trash-icon-ex", function (e) {
-	    e.preventDefault();
-	    var name = $(this).parent().parent().children().first().html();
-	    var exercises = JSON.parse(localStorage.getItem('exercise-calories'));
-	    exercises.forEach(function (object) {
-	      if (object.exerciseName == name) {
-	        exercises.splice(exercises.indexOf(object), 1);
-	      }
-	      exerciseJSON = JSON.stringify(exercises);
-	      localStorage.setItem('exercise-calories', exerciseJSON);
-	    });
-	    $(this).parent().parent().remove();
-	  });
-
-	  $(".calorie-cell-ex").on('click', function () {
-	    var key = "calorieCount";
-	    editValueEx(this, key);
-	  });
-
-	  $(".name-cell-ex").on('click', function () {
-	    var key = "exerciseName";
-	    editValueEx(this, key);
-	  });
-
-	  $("#filter").keyup(function () {
-	    filterExercises();
-	  });
-	});
-
-	$('#exercise-submit').on('click', function () {
-	  addExercise();
-	});
-
-	function emptyErrorsEx() {
-	  $("#error-message-id").empty();
-	}
-
-	function clearFieldsEx() {
-	  $('#name-field').val("");
-	  $('#calorie-field').val("");
-	}
-
-	function addExercise() {
-	  emptyErrorsEx();
-	  var exerciseName = $('#name-field').val();
-	  var calorieCount = $('#calorie-field').val();
-	  var newExercise = new Exercise(exerciseName, calorieCount);
-
-	  if (exerciseName === "") {
-	    $("#error-message-id").append("Please enter an exercise name");
-	  } else if (calorieCount === "") {
-	    $("#error-message-id").append("Please enter a calorie amount");
+	Exercise.prototype.addExercise = function () {
+	  var reset = new Reset();
+	  reset.emptyErrors(exerciseForm);
+	  this.name = $('#name-field').val();
+	  this.calories = $('#calorie-field').val();
+	  if (this.name === "") {
+	    $("#exercise-error").append("Please enter an exercise name");
+	  } else if (this.calories === "") {
+	    $("#calories-error").append("Please enter a calorie amount");
 	  } else {
-	    newExercise.build();
-	    newExercise.store();
-	    clearFieldsEx();
+	    reset.clearFields('#name-field', '#calorie-field');
+	    this.storeExercises();
+	    reset.clearTable(exercisesTable);
+	    this.displayExercises();
 	  }
-	}
+	};
 
-	function buildTableEx(name, calories) {
-	  var newRow = document.createElement('tr');
-	  buildNameCellEx(name, newRow);
-	  buildCalorieCellEx(calories, newRow);
-	  buildTrashCellEx(newRow);
-	  exercisesTable.insertBefore(newRow, exercisesTable.children[1]);
-	}
-
-	function buildNameCellEx(name, newRow) {
-	  var nameCell = document.createElement('td');
-	  nameCell.innerText = name;
-	  nameCell.className = "name-cell-ex";
-	  nameCell.id = "name-cell-id";
-	  newRow.appendChild(nameCell);
-	}
-
-	function buildCalorieCellEx(calories, newRow) {
-	  var calorieCell = document.createElement('td');
-	  calorieCell.innerText = calories;
-	  calorieCell.className = "calorie-cell-ex";
-	  calorieCell.id = "calorie-cell-id";
-	  newRow.appendChild(calorieCell);
-	}
-
-	function buildTrashCellEx(newRow) {
-	  var trashCell = document.createElement('td');
-	  trashCell.innerHTML = "<a href='' id='trash-icon-ex'><span class='glyphicon glyphicon-trash trash-icon'></a>";
-	  newRow.appendChild(trashCell);
-	}
-
-	function storeExercises(exerciseName, calorieCount) {
-	  var exerciseDataJSON = localStorage.getItem('exercise-calories');
-	  if (exerciseDataJSON === null) {
-	    exerciseDataJSON = '[]';
-	  }
-	  var exercises = JSON.parse(exerciseDataJSON);
-	  exercises.push({ exerciseName: exerciseName, calorieCount: calorieCount });
-	  exerciseDataJSON = JSON.stringify(exercises);
-	  localStorage.setItem('exercise-calories', exerciseDataJSON);
-	}
-
-	function displayExercises() {
-	  JSON.parse(localStorage.getItem('exercise-calories')).forEach(function (element) {
-	    buildTableEx(element.exerciseName, element.calorieCount);
+	Exercise.prototype.deleteExercise = function () {
+	  var name = this.name;
+	  var calories = this.calories;
+	  var exercises = JSON.parse(localStorage.getItem('exercise-calories'));
+	  exercises.forEach(function (object) {
+	    if (object.name === name && object.calories === calories) {
+	      exercises.splice(exercises.indexOf(object), 1);
+	    }
+	    exercisesJSON = JSON.stringify(exercises);
+	    localStorage.setItem('exercise-calories', exercisesJSON);
 	  });
-	}
+	};
 
-	function editValueEx(obj, key) {
+	Exercise.prototype.editValue = function (obj, key) {
+	  var name = this.name;
+	  var calories = this.calories;
+	  var type = 'exercise-calories';
+	  var save = new Save();
 	  obj.contentEditable = true;
 	  clickedEntry = obj;
 	  var originalKey = obj.innerText;
@@ -775,80 +730,80 @@
 	    if (event.keyCode == 13) {
 	      var newKey = obj.innerText;
 	      obj.contentEditable = false;
-	      if (key === "exerciseName") {
-	        setExNameLocalStorage(originalKey, newKey);
-	      } else {
-	        setExCalsLocalStorage(originalKey, newKey);
-	      }
+	      save.setLocalStorage(key, newKey, name, calories, type);
 	    }
 	  });
 	  $(document).click(function (e) {
 	    var newKey = clickedEntry.innerText;
 	    clickedEntry.contentEditable = false;
-	    if (key === "exerciseName") {
-	      setExNameLocalStorage(originalKey, newKey);
-	    } else {
-	      setExCalsLocalStorage(originalKey, newKey);
-	    }
+	    save.setLocalStorage(key, newKey, name, calories, type);
 	  });
-	}
+	};
 
-	function setExNameLocalStorage(originalKey, newKey) {
-	  var exercises = JSON.parse(localStorage.getItem('exercise-calories'));
-	  exercises.forEach(function (object, key) {
-	    if (object.exerciseName == originalKey) {
-	      object.exerciseName = newKey;
-	    }
-	    exerciseJSON = JSON.stringify(exercises);
-	    localStorage.setItem('exercise-calories', exerciseJSON);
-	  });
-	}
-
-	function setExCalsLocalStorage(originalKey, newKey) {
-	  var exercises = JSON.parse(localStorage.getItem('exercise-calories'));
-	  exercises.forEach(function (object, key) {
-	    if (object.calorieCount == originalKey) {
-	      object.calorieCount = newKey;
-	    }
-	    exerciseJSON = JSON.stringify(exercises);
-	    localStorage.setItem('exercise-calories', exerciseJSON);
-	  });
-	}
-
-	function filterExercises() {
-	  var input, filter, table, tr, td, i;
-	  input = document.getElementById("filter-field");
-	  filter = input.value.toUpperCase();
-	  tr = foodsTable.getElementsByTagName("tr");
-
-	  for (i = 0; i < tr.length; i++) {
-	    td = tr[i].getElementsByTagName("td")[0];
-	    if (td) {
-	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-	        tr[i].style.display = "";
-	      } else {
-	        tr[i].style.display = "none";
-	      }
-	    }
+	Exercise.prototype.storeExercises = function () {
+	  var exerciseDataJSON = localStorage.getItem('exercise-calories');
+	  if (exerciseDataJSON === null) {
+	    exerciseDataJSON = '[]';
 	  }
-	}
-
-/***/ },
-/* 3 */
-/***/ function(module, exports) {
-
-	function Reset() {}
-
-	Reset.prototype.emptyErrors = function (idName) {
-	  $(idName).children().children().empty();
+	  var exercises = JSON.parse(exerciseDataJSON);
+	  exercises.push(this);
+	  exerciseDataJSON = JSON.stringify(exercises);
+	  localStorage.setItem('exercise-calories', exerciseDataJSON);
 	};
 
-	Reset.prototype.clearFields = function (field1, field2) {
-	  $(field1).val("");
-	  $(field2).val("");
+	Exercise.prototype.displayExercises = function () {
+	  var exercises = JSON.parse(localStorage.getItem('exercise-calories'));
+	  exercises.forEach(function (object) {
+	    $('#all-exercises-table tr:first-child').after(`<tr>
+	      <td class='name-cell' id='exercise-name-cell'>${object.name}</td>
+	      <td class='calorie-cell' id='exercise-calorie-cell'>${object.calories}</td>
+	      <td><a href='' id='exercise-trash-icon'><span class='glyphicon glyphicon-trash trash-icon'></a></td>
+	      </tr>`);
+	  });
 	};
 
-	module.exports = Reset;
+	$(document).ready(function () {
+	  var newExercise = new Exercise();
+
+	  newExercise.displayExercises();
+
+	  $(document).on('click', '#exercise-trash-icon', function (e) {
+	    e.preventDefault();
+	    var name = $(this).parent().siblings()[0].innerHTML;
+	    var calories = $(this).parent().siblings()[1].innerHTML;
+	    $(this).parent().parent().remove();
+	    var newExercise = new Exercise(name, calories);
+	    newExercise.deleteExercise();
+	  });
+
+	  $(".calorie-cell").on('click', function () {
+	    var key = "calories";
+	    var $calories = $(this).html();
+	    var $name = $(this).siblings().html();
+	    editExercise = new Exercise($name, $calories);
+	    editExercise.editValue(this, key);
+	  });
+
+	  $(".name-cell").on('click', function () {
+	    var key = "name";
+	    var $name = $(this).html();
+	    var $calories = $(this).siblings().html();
+	    editExercise = new Exercise($name, $calories);
+	    editExercise.editValue(this, key);
+	  });
+
+	  $("#filter-field").keyup(function () {
+	    view = new View();
+	    view.filterItems(exercisesTable, this);
+	  });
+	});
+
+	$('#exercise-submit').on('click', function () {
+	  var newExercise = new Exercise();
+	  newExercise.addExercise();
+	});
+
+	module.exports = Exercise;
 
 /***/ }
 /******/ ]);
